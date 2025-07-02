a1f996b5816be420e510136f2fc8870c343af1ca
Yang Sijun <yang@sijun.dev>
2025-07-02 18:31:16 +0900

    Init xcode project

---
+++ HagaBible/HagaBible/HagaBibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HagaBibleApp.swift
+//  HagaBible
+//
+//  Created by 양시준 on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct HagaBibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
