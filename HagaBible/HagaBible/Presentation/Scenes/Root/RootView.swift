e9fdc6ff5466f2160db272bd70cf8a7d503537bc
Yang Sijun <yang@sijun.dev>
2025-07-07 23:39:11 +0900

    Organize folders

---
+++ HagaBible/HagaBible/Presentation/Scenes/Root/RootView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  HagaBible
+//
+//  Created by 양시준 on 7/2/25.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        TabView {
+            Tab("BibleReader", systemImage: "book.fill") {
+                BibleReaderView()
+            }
+            Tab("Recordings", systemImage: "waveform") {
+                RecordingsView()
+            }
+            Tab(role: .search) {
+                SearchView()
+            }
+        }
+        .tabBarMinimizeBehavior(.onScrollDown)
+    }
+}
+
+#Preview {
+    RootView()
+}
