45d5501d6c30289e24383ee012d6ad655588aafb
Yang Sijun <yang@sijun.dev>
2025-10-04 12:30:53 +0900

    Separate RecordingRepository Protocol and Implements

---
+++ HagaBible/HagaBible/Data/Repositories/Implementation/DefaultRecordingRepository.swift
@@ -0,0 +1,39 @@
+//
+//  DefaultRecordingRepository.swift
+//  HagaBible
+//
+//  Created by 양시준 on 10/4/25.
+//
+
+import Foundation
+import SwiftData
+
+@MainActor
+class DefaultRecordingRepository: RecordingRepository {
+    private let modelContext: ModelContext
+
+    init() {
+        let modelContainer = DIContainer.shared.resolve(type: ModelContainer.self)
+        self.modelContext = modelContainer.mainContext
+    }
+    
+    public func fetchRecordings() throws -> [Recording] {
+        let recordings = try modelContext.fetch(
+            FetchDescriptor<Recording>(
+                predicate: nil,
+                sortBy: [.init(\.createdAt, order: .reverse)]
+            )
+        )
+        return recordings
+    }
+
+    public func addRecording(_ recording: Recording) throws {
+        modelContext.insert(recording)
+        try modelContext.save()
+    }
+
+    public func deleteRecording(_ recording: Recording) throws {
+        modelContext.delete(recording)
+        try modelContext.save()
+    }
+}
