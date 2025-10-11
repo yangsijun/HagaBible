8524b10a02a7561309a03aeae4118ad8d9a2ab55
Yang Sijun <yang@sijun.dev>
2025-10-12 05:26:59 +0900

    Fix Error in Chapter and Verse Picker
    - Chapter and Verse Picker would not update immediately when selecting a BibleBook Picker

--- HagaBible/HagaBible/Presentation/Scenes/BibleReader/SubView/BibleNavigation/BibleNavigationView.swift
+++ HagaBible/HagaBible/Presentation/Scenes/BibleReader/SubView/BibleNavigation/BibleNavigationView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct BibleNavigationView: View {
     @Environment(\.dismiss) var dismiss
-    @Environment(BibleReaderViewModel.self) private var bibleReadeViewModel: BibleReaderViewModel
+    @Environment(BibleReaderViewModel.self) private var bibleReaderViewModel: BibleReaderViewModel
     
     @State private var viewModel: BibleNavigationViewModel = DIContainer.shared.resolve(type: BibleNavigationViewModel.self)
     
@@ -47,9 +47,15 @@ struct BibleNavigationView: View {
             }
             .task {
                 await viewModel.loadVersionList()
-                await viewModel.loadBookList(version: selectedVersion!)
-                await viewModel.loadChapterList(version: selectedVersion!, book: selectedBook!)
-                await viewModel.loadVerseList(version: selectedVersion!, book: selectedBook!, chapter: selectedChapter!)
+                if let selectedVersion = selectedVersion {
+                    await viewModel.loadBookList(version: selectedVersion)
+                    if let selectedBook = selectedBook {
+                        await viewModel.loadChapterList(version: selectedVersion, book: selectedBook)
+                        if let selectedChapter = selectedChapter {
+                            await viewModel.loadVerseList(version: selectedVersion, book: selectedBook, chapter: selectedChapter)
+                        }
+                    }
+                }
             }
             .onChange(of: selectedVersion) {
                 Task {
@@ -61,14 +67,20 @@ struct BibleNavigationView: View {
                 if oldValue?.bookCode == newValue?.bookCode {
                     selectedChapter = viewModel.chapterList.first(where: { $0.chapter == selectedChapter?.chapter })
                 } else {
-                    selectedChapter = viewModel.chapterList.first
+                    Task {
+                        await viewModel.loadChapterList(version: selectedVersion!, book: selectedBook!)
+                        selectedChapter = viewModel.chapterList.first
+                    }
                 }
             }
             .onChange(of: selectedChapter) { oldValue, newValue in
                 if oldValue?.chapter == newValue?.chapter {
                     selectedVerse = viewModel.verseList.first(where: { $0.verse == selectedVerse?.verse })
                 } else {
-                    selectedVerse = viewModel.verseList.first
+                    Task {
+                        await viewModel.loadVerseList(version: selectedVersion!, book: selectedBook!, chapter: selectedChapter!)
+                        selectedVerse = viewModel.verseList.first
+                    }
                 }
             }
             .toolbarTitleMenu {
@@ -98,26 +110,26 @@ struct BibleNavigationView: View {
                         }
                         
                         if let selectedVersion = selectedVersion {
-                            bibleReadeViewModel.versionCode = selectedVersion.versionCode
+                            bibleReaderViewModel.versionCode = selectedVersion.versionCode
                         }
                         if let selectedBook = selectedBook {
-                            bibleReadeViewModel.bookCode = selectedBook.bookCode
+                            bibleReaderViewModel.bookCode = selectedBook.bookCode
                         }
                         if let selectedChapter = selectedChapter {
-                            bibleReadeViewModel.chapterNum = selectedChapter.chapter
+                            bibleReaderViewModel.chapterNum = selectedChapter.chapter
                         }
                         if let selectedVerse = selectedVerse {
-                            bibleReadeViewModel.verseNum = selectedVerse.verse
-                            bibleReadeViewModel.navigatedVerseNum = selectedVerse.verse
+                            bibleReaderViewModel.verseNum = selectedVerse.verse
+                            bibleReaderViewModel.navigatedVerseNum = selectedVerse.verse
                         }
                         
                         dismiss()
                         if let selectedVerse = selectedVerse {
-                            bibleReadeViewModel.navigatedVerseNum = selectedVerse.verse
+                            bibleReaderViewModel.navigatedVerseNum = selectedVerse.verse
                         } else {
-                            bibleReadeViewModel.navigatedVerseNum = 1
+                            bibleReaderViewModel.navigatedVerseNum = 1
                         }
-                        bibleReadeViewModel.bibleNavigationUpdateTrigger.toggle()
+                        bibleReaderViewModel.bibleNavigationUpdateTrigger.toggle()
                     }) {
                         Image(systemName: "checkmark")
                     }
@@ -130,17 +142,20 @@ struct BibleNavigationView: View {
 }
 
 #Preview {
-    @Previewable @State var isPresented: Bool = false
-    @Previewable @State var viewModel = DIContainer.shared.resolve(type: BibleReaderViewModel.self)
+    DIContainer.registerForPreview()
+    var viewModel = DIContainer.shared.resolve(type: BibleReaderViewModel.self)
     
-    NavigationStack {
-        Button(action: { isPresented.toggle() }) {
-            Text("present")
-        }
-        .sheet(isPresented: $isPresented) {
-            BibleNavigationView()
-                .environment(viewModel)
-                .presentationDetents([.small])
-        }
+    return NavigationStack {
+        Text("")
+            .sheet(isPresented: .constant(true)) {
+                BibleNavigationView(
+                    selectedVersion: viewModel.bibleVersion,
+                    selectedBook: viewModel.bibleBook,
+                    selectedChapter: viewModel.bibleChapter,
+                    selectedVerse: viewModel.bibleVerse,
+                )
+                    .environment(viewModel)
+                    .presentationDetents([.small])
+            }
     }
 }
