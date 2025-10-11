bcd86ae84f0d48774f0a879eddc2c92c2dc36d2e
Yang Sijun <yang@sijun.dev>
2025-10-12 02:41:44 +0900

    Refactor BibleReaderView
    - Replace ZStack and Color to .background

--- HagaBible/HagaBible/Presentation/Scenes/BibleReader/BibleReaderView.swift
+++ HagaBible/HagaBible/Presentation/Scenes/BibleReader/BibleReaderView.swift
@@ -17,48 +17,45 @@ struct BibleReaderView: View {
     
     var body: some View {
         NavigationStack {
-            ZStack {
-                Color.gray.opacity(0.2)
-                    .ignoresSafeArea(.all)
-                ScrollViewReader { proxy in
-                    ScrollView {
-                        BibleVerseListView(
-                            verses: viewModel.bibleVerseList,
-                            language: viewModel.bibleVersion?.language ?? "English",
-                            fontConfiguration: fontThemeManager.fontConfiguration,
-                            theme: fontThemeManager.theme,
-                            highlightedVerseNum: viewModel.navigatedVerseNum
-                        )
-                        .onChange(of: viewModel.navigatedVerseNum) {
-                            if viewModel.navigatedVerseNum != nil {
-                                highlightTask?.cancel()
-                                highlightTask = Task {
-                                    try await Task.sleep(for: .seconds(3))
-                                    viewModel.navigatedVerseNum = nil
-                                }
-                            }
-                        }
-                    }
-                    .background(Color(uiColor: fontThemeManager.theme.backgroundColor))
-                    .swipeGesture(
-                        onLeftSwipe: {
-                            viewModel.goToPreviousChapter()
-                            viewModel.bibleNavigationUpdateTrigger.toggle()
-                        },
-                        onRightSwipe: {
-                            viewModel.goToNextChapter()
-                            viewModel.bibleNavigationUpdateTrigger.toggle()
-                        }
+            ScrollViewReader { proxy in
+                ScrollView {
+                    BibleVerseListView(
+                        verses: viewModel.bibleVerseList,
+                        language: viewModel.bibleVersion?.language ?? "English",
+                        fontConfiguration: fontThemeManager.fontConfiguration,
+                        theme: fontThemeManager.theme,
+                        highlightedVerseNum: viewModel.navigatedVerseNum
                     )
-                    .onChange(of: viewModel.bibleNavigationUpdateTrigger, initial: false) {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                            withAnimation(.linear(duration: 0.3)) {
-                                proxy.scrollTo((viewModel.navigatedVerseNum ?? 1) - 1, anchor: .top)
+                    .onChange(of: viewModel.navigatedVerseNum) {
+                        if viewModel.navigatedVerseNum != nil {
+                            highlightTask?.cancel()
+                            highlightTask = Task {
+                                try await Task.sleep(for: .seconds(3))
+                                viewModel.navigatedVerseNum = nil
                             }
                         }
                     }
                 }
+                .background(Color(uiColor: fontThemeManager.theme.backgroundColor))
+                .swipeGesture(
+                    onLeftSwipe: {
+                        viewModel.goToPreviousChapter()
+                        viewModel.bibleNavigationUpdateTrigger.toggle()
+                    },
+                    onRightSwipe: {
+                        viewModel.goToNextChapter()
+                        viewModel.bibleNavigationUpdateTrigger.toggle()
+                    }
+                )
+                .onChange(of: viewModel.bibleNavigationUpdateTrigger, initial: false) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        withAnimation(.linear(duration: 0.3)) {
+                            proxy.scrollTo((viewModel.navigatedVerseNum ?? 1) - 1, anchor: .top)
+                        }
+                    }
+                }
             }
+            .background(Color.gray.opacity(0.2), ignoresSafeAreaEdges: .all)
             .toolbarTitleDisplayMode(.inline)
             .toolbar {
                 BibleReaderToolbarContent(
