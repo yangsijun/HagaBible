eb1238416bbccbb51349924673fe8b1063a12420
Yang Sijun <yang@sijun.dev>
2025-10-18 21:41:54 +0900

    Implement Verses Text Share Feature in BibleReaderActionBar

--- HagaBible/HagaBible/Presentation/Scenes/BibleReader/SubView/ActionBar/BibleReaderActionBar.swift
+++ HagaBible/HagaBible/Presentation/Scenes/BibleReader/SubView/ActionBar/BibleReaderActionBar.swift
@@ -13,6 +13,22 @@ struct BibleReaderActionBar: View {
     var bookName: String
     var chapterNum: Int
     var bibleVerseList: [BibleVerse] = []
+    var bibleVersesString: String {
+        let start = selectStartIndex ?? 0
+        let end = selectEndIndex ?? start
+        
+        let referenceString = start == end
+        ? "[\(bookName) \(chapterNum):\(start + 1)]"
+        : "[\(bookName) \(chapterNum):\(start + 1)-\(end + 1)]"
+        
+        let verseLines: [String] = Array(start...end).map { idx in
+            "\(idx + 1) \(bibleVerseList[idx].verseText ?? "")"
+        }
+        let lines = [referenceString] + verseLines
+        let text = lines.joined(separator: "\n")
+        
+        return text
+    }
     
     var body: some View {
         if selectStartIndex != nil || selectEndIndex != nil {
@@ -31,6 +47,20 @@ struct BibleReaderActionBar: View {
                 .buttonStyle(.plain)
                 .frame(width: 44, height: 44)
                 .clipShape(.circle)
+                
+                ShareLink(
+                    item: bibleVersesString
+                ) {
+                    Circle()
+                        .foregroundStyle(.clear)
+                        .overlay(
+                            Image(systemName: "square.and.arrow.up")
+                        )
+                        .glassEffect(in: .circle)
+                }
+                .buttonStyle(.plain)
+                .frame(width: 44, height: 44)
+                .clipShape(.circle)
             }
             .padding(.horizontal, 30)
             .padding(.vertical, 8)
@@ -38,22 +68,18 @@ struct BibleReaderActionBar: View {
     }
     
     private func copyVerseTextToClipboard() {
-        let start = selectStartIndex ?? 0
-        let end = selectEndIndex ?? start
-        
-        let referenceString = start == end
-        ? "[\(bookName) \(chapterNum):\(start + 1)]"
-        : "[\(bookName) \(chapterNum):\(start + 1)-\(end + 1)]"
-        
-        let verseLines: [String] = Array(start...end).map { idx in
-            "\(idx + 1) \(bibleVerseList[idx].verseText ?? "")"
-        }
-        let lines = [referenceString] + verseLines
-        let text = lines.joined(separator: "\n")
-        UIPasteboard.general.string = text
+        UIPasteboard.general.string = bibleVersesString
         
         selectStartIndex = nil
         selectEndIndex = nil
     }
 }
 
+#Preview {
+    let bibleVerseList: [BibleVerse] = [
+        BibleVerse(bookCode: "GEN", bookOrder: 1, chapter: 1, verse: 1, verseText: "태초에 하나님이 천지를 창조하시니라", versionCode: "KRV"),
+        BibleVerse(bookCode: "GEN", bookOrder: 1, chapter: 1, verse: 2, verseText: "땅이 혼돈하고 공허하며 흑암이 깊음 위에 있고 하나님의 신은 수면에 운행하시니라", versionCode: "KRV"),
+        BibleVerse(bookCode: "GEN", bookOrder: 1, chapter: 1, verse: 3, verseText: "하나님이 가라사대 빛이 있으라 하시매 빛이 있었고", versionCode: "KRV"),
+    ]
+    BibleReaderActionBar(selectStartIndex: .constant(0), selectEndIndex: .constant(2), bookName: "창세기", chapterNum: 1, bibleVerseList: bibleVerseList)
+}
